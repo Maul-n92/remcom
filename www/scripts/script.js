6a282f066decde0f4eb0b6d676d91bc9a17867ea
Kirill Demidov <kademidov@gmail.com>
2022-09-24 18:02:53 +0300

    адаптив главной. Начали js

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,43 @@
-console.log('script work!');
+let petNames = ['Gans', 'Fiona', 'Kesha', 'Ryan'];
+
+console.log(  petNames  );
+
+for (let i = 0; i < petNames.length; i++) {
+  console.log( petNames[i] );
+
+  printHello(petNames[i]);
+
+  if (petNames[i].length < 5) {
+    console.log('Name: ' + petNames[i] + ' is short!');
+  } else {
+    console.log('Name: ' + petNames[i] + ' is long!');
+  }
+
+}
+
+
+
+
+let myName = ' ';
+
+console.log(myName.length);
+
+
+if (myName) {
+  console.log('Hello, ' + myName);
+} else {
+  console.log('Hello, tovarishc');
+}
+
+
+function printHello(name) {
+  if (name) {
+    console.log('Hello, ' + name);
+  } else {
+    console.log('Hello, tovarishc');
+  }
+}
+
+printHello();
+printHello('Enot');
+
