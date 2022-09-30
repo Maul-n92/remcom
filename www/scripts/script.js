5be95efb468d5672997cae954c03bb389c9ae6a1
Kirill Demidov <kademidov@gmail.com>
2022-09-30 19:01:44 +0300

    Разметка и стилизация всех страниц (баз адаптива). js - аккордионы, табы, фильтр

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -17,4 +17,58 @@ $(document).ready(function(){
   });
 
 
+  // Аккордеоны - часто задаваемые вопросы
+  let prevAccordionBtn;
+
+  $('.js-accordion-btn').on('click', function() {
+    if (prevAccordionBtn === this) {
+      $(this).next().slideToggle();
+    } else {
+      $('.js-accordion-btn').next().slideUp();
+      $(this).next().slideDown();
+      prevAccordionBtn = this;
+    }
+  });
+
+  // Табы в контактах
+  $('.js-tabs-link').on('click', function(event){
+    event.preventDefault();
+
+    $('.js-tabs-link').removeClass('active');
+    $(this).addClass('active');
+
+    const index = $(this).index('.js-tabs-link');
+
+    $('.js-tabs-content').removeClass('active');
+    $('.js-tabs-content').eq(index).addClass('active');
+  });
+
+  // Фильтр в Работах
+  $('.js-filter-link').on('click', function(event){
+    event.preventDefault();
+
+    $('.js-filter-link').removeClass('active');
+    $(this).addClass('active');
+
+    let dataFilter = $(this).data('filter');
+    console.log(dataFilter);
+
+    if (dataFilter === 'all') {
+      $('.js-works-item').show();
+      return;
+    }
+
+    $('.js-works-item').each(function(){
+      let dataType = $(this).data('type');
+      console.log(dataType);
+
+      if (dataFilter === dataType) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+
+
+  });
 });
